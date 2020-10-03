3dc0b5b7b47dad04f3a1a054e46d136acbf02603
Hilmaww <hilmi.ambari27@gmail.com>
2020-10-03 21:36:29 +0800

    from react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+            <div className="container">
+              <div className="frame">
+                <section className="header">
+                  <button className="header-btn main-black-color">Add</button>
+                  <h1 className="header-title">todo list</h1>
+                  <button className="header-btn main-red-color">Clear</button>
+                </section>
+                <section className="add">
+                    <form className="add-form">
+                        <input type="form" className="add-input"/>
+                        <button className="add-btn main-black-color">Add</button>
+                    </form>
+                </section>
+                <section className="todos">
+                  <div className="todo">
+                    <span className="text-todo">Learning React</span>
+                  </div>
+                </section>
+              </div>
+            </div>
   );
 }
 
